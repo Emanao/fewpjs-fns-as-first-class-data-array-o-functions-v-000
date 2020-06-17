fb1cbc7b464ce7004e22b41733799947a75d190c
Eva Navarro <emanao@me.com>
2020-06-17 18:22:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+let routine = []
+routine[0] = function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+routine[1]=function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
