f5484227ecb213263659c1b1c4055852c6cdaa2d
Eva Navarro <emanao@me.com>
2020-06-17 18:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ wakeDog= function (dogName, dogBreed) {
   return`Wake ${dogName} the ${dogBreed}`;
 }
 routine[1]=function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`;
 }
 routine[2] = function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
 routine[3]=function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 routine[4] = function walkHome(dogName, dogBreed) {
-  console.log(`Walk home wit ${dogName} the ${dogBreed}`);
+  return `Walk home wit ${dogName} the ${dogBreed}`;
 }
 routine[5]=function unleashDog(dogName, dogBreed) {
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
