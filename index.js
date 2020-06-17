fd38f243189e71c38395295b27d972cb48e7c124
Eva Navarro <emanao@me.com>
2020-06-17 18:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let routine = []
-wakeDog = function (dogName, dogBreed) {
+const wakeDog = function (dogName, dogBreed) {
   return`Wake ${dogName} the ${dogBreed}`;
 }
-leashDog = function (dogName, dogBreed) {
+const leashDog = function (dogName, dogBreed) {
   return `Leash ${dogName} the ${dogBreed}`;
 }
-walkToPark = function (dogName, dogBreed) {
+const walkToPark = function (dogName, dogBreed) {
   return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
-throwFrisbee = function (dogName, dogBreed) {
+const throwFrisbee = function (dogName, dogBreed) {
   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
-walkHome = function (dogName, dogBreed) {
+const walkHome = function (dogName, dogBreed) {
   return `Walk home wit ${dogName} the ${dogBreed}`;
 }
-unleashDog = function (dogName, dogBreed) {
+const unleashDog = function (dogName, dogBreed) {
   return `Unleash ${dogName} the ${dogBreed}`;
 }
